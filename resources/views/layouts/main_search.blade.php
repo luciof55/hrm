16314be0a504f5429c9a95ee0b112e4d80da0289
Top Group <gitlab@topgroup.com.ar>
2019-03-20 14:13:07 -0300

    Add search option. Change info color

--- resources/views/layouts/main_search.blade.php
+++ resources/views/layouts/main_search.blade.php
@@ -1,7 +1,7 @@
-<form class="form-inline" action="#" method="GET">
+<form class="form-inline" action="{{action('Administration\WorkflowController@index')}}" method="GET">
   @csrf
   <div class="input-group">
-    <input type="search" class="form-control" placeholder="@lang('messages.Search')" aria-label="@lang('messages.Search')" aria-describedby="basic-addon2">
+    <input type="search" class="form-control" placeholder="@lang('messages.Search')" aria-label="@lang('messages.Search')" aria-describedby="basic-addon2" id="name_filter" name="name_filter">
     <div class="input-group-append">
       <button class="btn btn-success" type="submit">@lang('messages.Search')</button>
     </div>
