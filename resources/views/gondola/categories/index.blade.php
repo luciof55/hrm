a7f4294802947f24b11e4575624f1ce1134b4fa0
Top Group <gitlab@topgroup.com.ar>
2019-03-27 21:18:27 -0300

    Minor fix

--- resources/views/gondola/categories/index.blade.php
+++ resources/views/gondola/categories/index.blade.php
@@ -73,9 +73,7 @@
 								<div class="d-none d-md-block p-1"><button id="button_export" type="button" onclick="location.href='{{$actionExport}}';" class="btn btn-info btn-md"><i class="pr-2 fa fa-th-list"></i>@lang('messages.Export')</button></div>
 							@endif
 							<div class="p-1 ml-auto">
-								<div class="btn-group">
-									@include('admin.only_buttons', ['btn_new' => true, 'btn_view' => true, 'btn_edit' => true, 'btn_enable' => true, 'btn_remove' => true])
-								</div>
+								@include('admin.buttons', ['btn_new' => true, 'btn_view' => true, 'btn_edit' => true, 'btn_enable' => true, 'btn_remove' => true])
 							</div>
 						</div>
 					</div>
